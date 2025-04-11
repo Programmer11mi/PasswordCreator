8329fc03e0af32097a9636b78bb82fae01df2ae4
Programmer11mi <165056853+Programmer11mi@users.noreply.github.com>
2025-04-11 16:58:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ tg.expand();
 function sendData() {
     const length = document.getElementById('numInput').value;
     
-    if (length >= 8 && length <= 60) {
+    if (length >= 8 && length <= 30) {
         tg.sendData(JSON.stringify({ length: length }));
     } else {
-        alert("Будь ласка, введіть кількість символів від 8 до 60.");
+        alert("Будь ласка, введіть кількість символів від 8 до 30.");
     }
 }
+
+document.getElementById('sendButton').addEventListener('click', sendData);
