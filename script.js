e2c2afda7934d7147676c59daca030dff6be11c0
Programmer11mi <165056853+Programmer11mi@users.noreply.github.com>
2025-04-11 17:11:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-let tg = window.Telegram.WebApp;
-tg.expand();
+document.addEventListener("DOMContentLoaded", () => {
+    let tg = window.Telegram.WebApp;
+    tg.expand();
 
-function sendData() {
-    const length = document.getElementById('numInput').value;
-    
-    if (length >= 8 && length <= 30) {
-        tg.sendData(JSON.stringify({ length: length }));
-    } else {
-        alert("Будь ласка, введіть кількість символів від 8 до 30.");
+    function sendData() {
+        const input = document.getElementById('numInput');
+        const length = parseInt(input.value);
+
+        if (length >= 8 && length <= 60) {
+            tg.sendData(JSON.stringify({ length: length }));
+        } else {
+            alert("Введіть число від 8 до 60.");
+            input.value = '';
+            input.focus();
+        }
     }
-}
 
-document.getElementById('sendButton').addEventListener('click', sendData);
+    document.getElementById('sendButton').addEventListener('click', sendData);
+});
